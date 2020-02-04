fbf0561418f50e40c5458e09310ac0a2251788f5
Dario Fernandez <dariofernandez@hotmail.com>
2020-02-04 12:29:54 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProjectNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
